3a0f46ed0b027007668773a17853502f1d562a54
venkatesh16031999 <rvenki666@gmail.com>
2019-08-01 23:00:04 +0530

    added

--- adminportal/assets/Vue.js
+++ adminportal/assets/Vue.js
@@ -8,11 +8,23 @@ var drop=new Vue({
         lists:[
             {
                 name:"yadhu",
-                age:'21',
+                movingfrom:"19th, A Ejipura, BangaloreCross",
+                movingto:"19th, A Ejipura, BangaloreCross",
+                movingdistance:"Moving Distance 50 KM",
+                dateofshifting:"23/06/2019",
+                movetype:"2 BHK",
+                totalitems:"25",
+                number:"678987654",
                 items:[
                     'cot 1',
                     'table 2',
-                    'chair 4' 
+                    'chair 4',
+                    'sofa',
+                    'TV' ,
+                    'chair 4',
+                    'sofa',
+                    'TV',
+                    'light'
                 ]
             },
             {
@@ -53,27 +65,76 @@ var drop=new Vue({
         lists2:[
             {
                 name:"yadhu2",
-                age:'21',
-                quote:'21',
+                movingfrom2:"19th, A Ejipura, BangaloreCross",
+                movingto:"19th, A Ejipura, BangaloreCross",
+                movingdistance2:"Moving Distance 50 KM",
+                name:"edwin1",
+                dateofshifting2:"Jul.23",
+                movingtype2:" 2 BHK",
+                totalitems2:" 25",
+                number2:"6789876546",
+                requestedtime2:"5pm",
+                vendors2:[
+                    {
+                        name:"venkatesh",
+                        contact:"3456543456",
+                        quote:'20000',
+                    },
+                    {
+                        name:"venkatesh",
+                        contact:"3456543456",
+                        quote:''
+                    },
+                    {
+                        name:"venkatesh",
+                        contact:"3456543456",
+                        quote:''
+                    },
+                    {
+                        name:"venkatesh",
+                        contact:"3456543456",
+                        quote:''
+                    }
+                ]
             },
             {
                 name:"venkatesh2",
                 age:'21',
-                quote:'22'
+                date:'13.08.2018',
+                vendors2:[],
+                quote:''
             },
             {
                 name:"venkat2",
                 age:'21',
+                date:'14.08.2018',
+                vendors2:[],
                 quote:""
             },
             {
                 name:"ve2",
                 age:'21',
-                quote:""
+                date:'15.08.2018',
+                vendors2:[],
+                quote:"20000"
             }
         ],
         lists3:[
-            {name:"yadhu23",age:'21',},
+            {
+                name:"yadhu23",
+                date:"july 3",
+                movingfrom3:"19th, A Ejipura, BangaloreCross",
+                movingto3:"19th, A Ejipura, BangaloreCross",
+                movingdistance3:"Moving Distance 50 KM",
+                dateofshifting3:"July 23",
+                movetype3:"2 BHK",
+                totalitems3:"25",
+                companyconformedfor3:"Plan Packers And Movers ",
+                numberofcompany3:"9487874439",
+                amount3:"18000 ",
+
+
+            },
             {name:"venkatesh23",age:'21'},
             {name:"venkat23",age:'21'},
             {name:"ve23",age:'21',},
@@ -82,7 +143,13 @@ var drop=new Vue({
         lists4:[
             {
                 name:"yadhu24",
-                age:'21',
+                movingfrom4:"19th, A Ejipura, BangaloreCross",
+                movingto4:"19th, A Ejipura, BangaloreCross",
+                movingdistance4:"Moving Distance 50 KM",
+                dateofshifting4:"23/06/2019",
+                movingtype4:"2 BHK",
+                totalitems4:"25",
+                Cancelledon4:"6pm",
             },
             {
                 name:"venkatesh24",
@@ -116,7 +183,7 @@ var drop=new Vue({
         //    ids1.style.display=this.display1;
         //    this.display="block"
            
-
+        console.log( this.lists2);
         },
         drop2(id2){
             var ids2 = document.getElementById(id2);
@@ -133,8 +200,18 @@ var drop=new Vue({
         },  drop22(id2){
             var ids2 = document.getElementById(id2);
             // this.height="100px";
-        }
+        },
+        
+        
     },
+   
+            
+            
+        
+  
+    
+
+
     // created:function (){
     //     for(let list in this.lists2){
     //     if(this.lists2[list].quote!==""){
